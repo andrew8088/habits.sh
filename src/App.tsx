410d65ade030e52b8acfaa5a6ea7897e13026cb7
Andrew Burgess <andrew.burgess@classdojo.com>
2023-07-20 09:06:41 -0400

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type { Component } from "solid-js";
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class="flex flex-col">
+      <h1 class="text-3xl font-bold text-center m-3">My Habits</h1>
     </div>
   );
 };
